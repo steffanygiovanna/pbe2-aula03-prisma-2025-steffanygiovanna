1e30ce42974017a2935ea4b31de00ef8d691843c
steffanygiovanna <00001108194278sp@al.educacao.sp.gov.br>
2025-03-26 17:01:33 -0300

    mm

---
+++ front/script.js
@@ -0,0 +1,23 @@
+
+const uri = './assets/dados.json';
+var produtos = [];
+
+fetch(uri)
+      .then(resp => resp.json())
+      .then(resp => { produtos = resp; })
+      .then(() => mostrarCards())
+     
+
+      function mostrarCards() {
+        const main = document.querySelector('main');
+        produtos.forEach(p => {
+            main.innerHTML += `
+             <div class="card">
+            <h3>${p.produto}</h3>
+            <img src="${p.imagem}" alt="${p.produto}">
+            <p>Preco: ${p.preco}</p>
+            <button>Detalhes</button>
+        </div>
+            `;
+        })
+      }
